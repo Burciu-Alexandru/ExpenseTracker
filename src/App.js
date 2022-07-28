9dd4f26c022843061fc60c4daa67e164da0a2fdf
Burciu-Alexandru <72673169+Burciu-Alexandru@users.noreply.github.com>
2022-07-28 21:02:04 +0300

    #1: Added the static expenses list whit some comic names :)

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import ExpensesList from "./components/Expenses/ExpensesList";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Hartie Igienica",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "Tv cu diagonala sa moara dujmani de ciuda", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Asigurare la Cazan",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Birou Fulger Mcqueen",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>this is also visible</p>
+      <ExpensesList expenses={expenses} />
+    </div>
+  );
+};
+
+export default App;
