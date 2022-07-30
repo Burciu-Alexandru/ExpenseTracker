4c7e2418350960235f4bda30fd18fb195b838d49
Burciu-Alexandru <72673169+Burciu-Alexandru@users.noreply.github.com>
2022-07-30 23:41:13 +0300

    #10 Added commenst to App and NewExpenses component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
+// My first react app done during the first part of Max Schwarzmüller
+// An expense tracker app where you can your see expense filtered by year(until now)
+// see a visual representation of how much you have speend by month and add new expenses
+
+// One future improvement of the app could be that when you add a new expense it should be really 
+// stored local:), until now it only store as long as you don't close the app :) 
+
+// Really like how it look and learned a lot, one big thing I've learned while doing this app
+// is that I should really be more focus on typos :)), that were my main stress on debugging
+// what I mean by that is that verry often the source of my bug was not my logic but my typos :)
+
 import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import ExpensesList from './components/Expenses/ExpensesList';
 
+
+//dummy state for testing, sorry for the names :))
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
+    title: 'Sprai',
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'Tv cu diagonala sa moara dujmanii', amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
-    title: 'Car Insurance',
+    title: 'Asigurare la cazan',
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
     id: 'e4',
-    title: 'New Desk (Wooden)',
+    title: 'Birou fulger Mcqueen',
     amount: 450,
     date: new Date(2021, 5, 12),
   },
 ];
 
 const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);// state of the expenses, all the expenses are stored here
 
   const addExpenseHandler = (expense) => {
     console.log(expense);
@@ -35,13 +48,16 @@ const App = () => {
     });
     console.log(expenses);
   };
+  //function to add new expense, where I take an object named expense, and use array a copy of the state
+  //and spread operator to update the state
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
+      <NewExpense onAddExpense={addExpenseHandler} /> 
       <ExpensesList expenses={expenses} />
     </div>
   );
+  // here i render my NewExpense and ExpensesList components, both whit comments on their files
 };
 
 export default App;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,14 @@
+//The role of new expense is to add the functionality of adding new expenses to our list 
+//of expenses here I handle the function that send back the object to the App component for
+//there to be added to the expenses
+
 import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-  const [addNew, setAddNew] = useState(false);
+  const [addNew, setAddNew] = useState(false);// a state used for showing and hiding the expense form(kiinda like a modal but not quite)
 
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
@@ -13,16 +17,23 @@ const NewExpense = (props) => {
     };
     console.log('new expense');
     props.onAddExpense(expenseData);
-    //setAddNew(false);
+    setAddNew(false);
   };
+  // here on saveExpenseDataHandler take the object from the form and using the spread operator
+  // create a new object whit an ID then, I set the state to false so it won't show
+
+  // I just tought right now that is not really a good idea cuz maybe someone what to add multiple 
+  // expenses at once and this functionality could be a littel bit of pain in the ass
 
   const startEditinHanler = () => {
     setAddNew(true);
   };
+  // function to set the state to true
 
   const stopEditingHanlder = () => {
     setAddNew(false);
   };
+  // function to set the state to false
 
   return (
     <div className='new-expense'>
@@ -38,4 +49,6 @@ const NewExpense = (props) => {
     </div>
   );
 };
+
+// on return I use conditonal rendering for hiding and showint the form
 export default NewExpense;
