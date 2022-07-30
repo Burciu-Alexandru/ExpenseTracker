3d3cc5a53e7a76e49ed15c5d2446a773c1237026
Burciu-Alexandru <72673169+Burciu-Alexandru@users.noreply.github.com>
2022-07-30 14:09:26 +0300

    #5 added logic for the filter, now you can filter by year the expenses

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -7,26 +7,38 @@ import ExpensesFilter from "../NewExpense/ExpenseFilter";
 //prima tema
 //a doua tema
 const ExpensesList = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  const subarray = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent =  <p>Nu ai cheltuit nimic sarakule luna asta</p>
+
+  if(subarray.length > 0 ) {
+    expensesContent = subarray.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  
+  }
+
   return (
     <div>
-      <Card className='expenses'>
+      <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
+          onGetFileteredData={filterChangeHandler}
         />
-        {props.expenses.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
